2885e6b7bb60cb85f8345357706339b4ec86dd85
macbook <macbook@macbooks-MacBook-Pro.local>
2023-06-16 13:17:18 +0000

    complete build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import bg from "./images/apple-splash.png"
+import Header from './components/Header';
 
 function App() {
+
+  const styles = {
+    backgroundImage: `url(${bg})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='h-screen w-screen' style={styles}>
+      <Header />
+
+      <div className='h-full w-full flex flex-col justify-center px-[4rem] ' >
+          <h1 className='text-[#98EC65] font-bold text-[5rem]' >Creative</h1>
+          <h3 className='text-white text-[2rem] font-bold ' >design studio</h3>
+
+          <button className='w-[10rem] py-[6px] rounded-3xl bg-[#98EC65] hover:bg-[#81E047] mt-[1rem]' >Explore Now</button>
+      </div>
+
+    </main>
   );
 }
 
